dcbe7100e7efbcf81c11bc4241f32598e11ade44
Ansam1999 <ansamghoul99@gmail.com>
2021-02-15 21:31:33 +0200

    kk

--- App.js
+++ App.js
@@ -1,16 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
-<<<<<<< HEAD
-import Login from './Pages/Login';
-import Main from './Pages/Main';
-import NewAccount from './Pages/NewAccount';
-
-
-export default function App() {
-  return (
-   <NewAccount />
-=======
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import Login from './Pages/Login';
 import Main from './Pages/Main';
@@ -21,14 +11,13 @@ import { NavigationContainer } from '@react-navigation/native';
 const Drawer = createDrawerNavigator();
 
 export default function App() {
+ 
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName = 'Home'>
         <Drawer.Screen name = 'Home' component ={MainTabScreen} ></Drawer.Screen>
       </Drawer.Navigator>
     </NavigationContainer>
->>>>>>> 2964f87 (roaafirst)
+
   );
 }
-
-
--- Pages/HomeScreen.js
+++ Pages/HomeScreen.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { View ,Text} from 'react-native';
+import { View ,Text,Image} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 function HomeScreen(props) {
     return (
-        <View>
-            <Text>Home</Text>
+        <View style={{flex:1,alignItems:'center',backgroundColor:'#E6EAEC'}}>
+        
+        <View style={{flexDirection:'column',alignItems:'center',width:'93%',backgroundColor:'white',
+        borderRadius:15,borderColor:'#E6EAEC',borderWidth:1,marginTop:15}}>
+            <Image source={require('../assets/k1.jpg')} style={{width:'100%',height:145,borderRadius:15}}/>
+            
+            <Text style={{marginTop:20,fontSize:24,color:'#2F6F99', fontFamily:'Roboto'}}>Kindergarten Name</Text>
+            <View style={{flexDirection:'row',marginTop:10,marginBottom:25}}>
+                <Icon name="location-outline" size={20} style={{marginRight:-50}}  />
+                <View style={{flexDirection:'column',alignItems:'center'}}>
+            <Text style={{fontSize:20,color:'#8D9092'}}>Nablus</Text>
+            <Text style={{fontSize:16,color:'#8D9092'}}>An-Najah National University street</Text>
+            </View>
+            </View>
+            </View>
         </View>
     );
 }
